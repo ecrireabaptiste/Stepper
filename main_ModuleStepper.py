d62788170c65e5596ec754da9fbadc2217a8188e
ecrireabaptiste <34769059+ecrireabaptiste@users.noreply.github.com>
2021-04-13 19:09:53 +0100

    Update main_ModuleStepper.py
    Added M0, M1, M2 and microsteps in the instantiation

--- main_ModuleStepper.py
+++ main_ModuleStepper.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
-stepper1 = ModuleStepper.Stepper(20,21,16)
+stepper1 = ModuleStepper.Stepper(20,21,16,12,7,8,32)
 stepper1.step(0,0.5,2) #dir, revs, rps
 stepper1.step(1,1,4) #dir, revs, rps
 stepper1.step(0,2,8) #dir, revs, rps
