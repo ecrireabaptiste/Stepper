23c5605930bfde6329242b62a399c5b367f5c080
ecrireabaptiste <34769059+ecrireabaptiste@users.noreply.github.com>
2021-04-13 19:13:48 +0100

    Update main_ModuleStepper.py

--- main_ModuleStepper.py
+++ main_ModuleStepper.py
@@ -3,9 +3,9 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
-stepper1 = ModuleStepper.Stepper(20,21,16,12,7,8,32)
-stepper1.step(0,0.5,2) #dir, revs, rps
-stepper1.step(1,1,4) #dir, revs, rps
-stepper1.step(0,2,8) #dir, revs, rps
+stepper1 = ModuleStepper.Stepper(20,21,16,12,7,8) # (pin_dir, pin_step, pin_sleep, pin_M0, pin_M1, pin_M2)
+stepper1.step(0,0.5,2,32) #dir, revs, rps, microstep
+stepper1.step(1,1,4,16) #dir, revs, rps, microstep
+stepper1.step(0,2,8,8) #dir, revs, rps, microstep
 
 GPIO.cleanup()
